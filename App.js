f6bb7af1247eee8c9bca84d7d64ee72b7418f59c
Ibrahim Mohamed <ibrahim.mohamedm@hotmail.com>
2019-06-08 19:56:22 -0600

    update

--- App.js
+++ App.js
@@ -1,3 +1,10 @@
 const nav = document.querySelector('#main');
 let topOfNav = nav.offsetTop;
 
+function fixNav() {
+  if (window.scrollY >= topOfNav) {
+    document.body.style.paddingTop = nav.offsetHeight + 'px';
+    document.body.classList.add('fixed-nav');
+  }
+}
+
